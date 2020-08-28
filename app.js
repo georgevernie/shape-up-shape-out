230c28f03ec27b8e73fa17a566d7cf45cfd8aee7
georgevernie <jonlangner@gmail.com>
2020-08-28 15:19:29 -0500

    Refactored and debugged rectangle, as well working the methods

--- app.js
+++ app.js
@@ -1,45 +1,24 @@
-//DOM for drawing shapes
-const SHAPECANVAS = document.getElementById("shape-canvas");
-const RECTANGLEINPUTHEIGHT = document.getElementById("rHeight");
-const RECTANGLEINPUTWIDTH = document.getElementById("rWidth");
-const DRAWRECTANGLE = document.getElementById("draw-rectangle");
-const SQUAREINPUT = document.getElementById("sqr-input");
-const DRAWSQUARE = document.getElementById("draw-square");
-const CIRCLEINPUT = document.getElementById("circle-input");
-const DRAWCIRCLE = document.getElementById("draw-circle");
-const TRIANGLEINPUT = document.getElementById("triangle-input");
-const DRAWTRIANGLE = document.getElementById("draw-triangle");
-
-//DOM for Shape Selection
-const SELECTEDSHAPENAME = document.getElementById("selected-shape-name");
-const SELECTEDSHAPEWIDTH = document.getElementById("selected-shape-width");
-const SELECTEDSHAPEHEIGHT = document.getElementById("selected-shape-height");
-const SELECTEDSHAPERADIUS = document.getElementById("selected-shape-radius");
-const SELECTEDSHAPEAREA = document.getElementById("selected-shape-area");
-const SELECTEDSHAPEPERIMETER = document.getElementById("selected-shape-perimeter");
-
 //MIN and MAX constants to define the limits of a shape in shape canvas
 const MAX = 600;
 const MIN = 0;
 
-//Generates Random Values for the absolute positioning of the shapes.
-function randValGenerator(min, max){
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
 // Shape Defines the basic properties of a shape, this where any click events take place, and where each 
-class shape {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
+class Shape {
+    constructor(_width, _height = null) {
+        this.width = _width;
+        this.height = _height;
         this.div = document.createElement("div");
+        //Generates Random Values for the absolute positioning of the shapes.
+        this.randValGenerator = function(min, max) {
+                return Math.floor(Math.random() * (max - min) + min);
+            };
         this.div.addEventListener("click", (e) => {
             //Preprocessing of common use variables prior to logic.
             let shapeHeight = parseInt(e.target.style.height);
             let shapeWidth = parseInt(e.target.style.width);
             let shapePerimeter = shapeHeight + shapeWidth + shapeHeight + shapeWidth;
             let shapeArea = shapeHeight * shapeWidth;
-            let shapeName = e.target.id;
+            let shapeName = e.target.className;
             //Square and rectangle are measured in the same ways.
             if(shapeName === "square" || shapeName === "rectangle"){
                 SELECTEDSHAPENAME.textContent = shapeName;
@@ -55,7 +34,7 @@ class shape {
                     SELECTEDSHAPENAME.textContent = shapeName;
                     SELECTEDSHAPEWIDTH.textContent = "N/A";
                     SELECTEDSHAPEHEIGHT.textContent = "N/A";
-                    SELECTEDSHAPERADIUS.textContent = shapeWidth; 
+                    SELECTEDSHAPERADIUS.textContent = shapeWidth / 2; 
                     SELECTEDSHAPEPERIMETER.textContent = 2 * Math.PI * shapeWidth;
                     SELECTEDSHAPEAREA.textContent = Math.PI * Math.pow(shapeWidth, 2);
                 break;
@@ -75,9 +54,18 @@ class shape {
                 break;
             }
         });
-        this.div.addEventListener("dblclick", (e) =>{
+    }
+    //removes a shape when double clicked and clears the shape information panel. 
+    deleteShape(){
+        this.div.addEventListener("dblclick", function(e){
             e.target.remove();
-        }); 
+            SELECTEDSHAPENAME.textContent = "";
+            SELECTEDSHAPEWIDTH.textContent = "";
+            SELECTEDSHAPEHEIGHT.textContent = "";
+            SELECTEDSHAPERADIUS.textContent = ""; 
+            SELECTEDSHAPEPERIMETER.textContent = "";
+            SELECTEDSHAPEAREA.textContent = "";
+        });
     }
     //Specifies a outside box which contains for each shape and keeps it within the shape canvas and is also used for debugging.
     generateBoundingBox(){
@@ -86,59 +74,62 @@ class shape {
         let boundingBox = this.div;
         let finalCanvasWidth = MAX - this.width - 5;   //-5 due to bounding box size.
         let finalCanvasHeight = MAX - this.width - 5; //-5 due to bounding box size.
-        if(this.div.id === "rectangle"){ //Rectangle requires a uniquely specificed boundingBox to keep it in bounds
+        if(this.div.firstElementChild.className === "rectangle"){ //Rectangle requires a uniquely specificed boundingBox to keep it in bounds
             boundingHeight = this.height + 5; 
-            finalCanvasHeight = this.height - 5;
+            finalCanvasHeight = MAX - this.height - 5;
         }   
-        boundingBox.style.marginTop = `${randValGenerator(MIN,finalCanvasHeight)}px`;
-        boundingBox.style.marginLeft =`${randValGenerator(MIN,finalCanvasWidth)}px`;
-        boundingBox.id = "boundingBox";
-        boundingBox.style.width = `${boundingWidth}px`;
+        boundingBox.style.marginTop = `${this.randValGenerator(MIN,finalCanvasHeight)}px`;
+        boundingBox.style.marginLeft =`${this.randValGenerator(MIN,finalCanvasWidth)}px`;
+        boundingBox.className = "boundingBox";
+        boundingBox.style.side = `${boundingWidth}px`;
         boundingBox.style.height = `${boundingHeight}px`;
         SHAPECANVAS.appendChild(boundingBox);
-    }  
+    } 
 }
 
-class square extends shape{
+class Square extends Shape{
     constructor(width){
-        super(width);
+        super(width,width); //width refers to the side length of the square
         this.renderSquare();
         this.generateBoundingBox();
+        this.deleteShape();
     }
 
     renderSquare(){
         let square = document.createElement("div");
-        square.id = "square";
+        square.className = "square";
         square.style.width = `${this.width}px`;
         square.style.height = `${this.width}px`;
         this.div.appendChild(square);
     }    
 }
 
-class rectangle extends shape{
+class Rectangle extends Shape{
     constructor(height,width){
         super(height,width);
         this.renderRectangle();
         this.generateBoundingBox();
+        this.deleteShape();
     }
     renderRectangle(){
         let rectangle = document.createElement("div");
-        rectangle.id = "rectangle";
+        rectangle.className = "rectangle";
         rectangle.style.width = `${this.width}px`;
         rectangle.style.height = `${this.height}px`;
         this.div.appendChild(rectangle);
     }
 }
 
-class circle extends shape{
+class Circle extends Shape{
     constructor(width){
-        super(width);
+        super(width, width); // width refers to diameter.
         this.renderCircle();
         this.generateBoundingBox();
+        this.deleteShape();
     }
     renderCircle(){
         let circle = document.createElement("div");
-        circle.id = "circle";
+        circle.className = "circle";
         circle.style.width = `${this.width}px`;
         circle.style.height = `${this.width}px`;
         circle.style.borderRadius = `${50}%`;
@@ -146,15 +137,16 @@ class circle extends shape{
     }
 }
 
-class triangle extends shape{
+class Triangle extends Shape{
     constructor(width){
-        super(width);
+        super(width, width);
         this.renderTriangle();
         this.generateBoundingBox();
+        this.deleteShape();
     }
     renderTriangle(){
         let triangle = document.createElement("div");
-        triangle.id = "triangle";
+        triangle.className = "triangle";
         triangle.style.borderLeft = `${0}px solid transparent`
         triangle.style.borderRight = `${this.width}px solid transparent`;
         triangle.style.borderBottom = `${this.width}px solid rgb(${255},${251},${0})`;
@@ -163,14 +155,14 @@ class triangle extends shape{
 }
 
 DRAWSQUARE.addEventListener("click", () => {
-    new square(parseInt(SQUAREINPUT.value));
+    new Square(parseInt(SQUAREINPUT.value));
 });
 DRAWRECTANGLE.addEventListener("click", () =>{
-    new rectangle(parseInt(RECTANGLEINPUTHEIGHT.value),parseInt(RECTANGLEINPUTWIDTH.value));
+    new Rectangle(parseInt(RECTANGLEINPUTHEIGHT.value),parseInt(RECTANGLEINPUTWIDTH.value));
 });
 DRAWCIRCLE.addEventListener("click", () => {
-    new circle(parseInt(CIRCLEINPUT.value));
+    new Circle(parseInt(CIRCLEINPUT.value));
 });
 DRAWTRIANGLE.addEventListener("click", () => {
-    new triangle(parseInt(TRIANGLEINPUT.value));
+    new Triangle(parseInt(TRIANGLEINPUT.value));
 });
